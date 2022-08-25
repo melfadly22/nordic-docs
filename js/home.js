70de282042f33252ab905c8dee4f5c1eff51bc09
melfadly22 <mohamed.elfadly@nordicglobal.com>
2022-08-25 07:40:28 -0400

    ME-8-25-2022-5

---
+++ js/home.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    location.href = "https://nordic-prod-docs.auth.us-east-1.amazoncognito.com/login?response_type=code&state=000140076001500027526054000060002030&client_id=1u61vscpnp1rp9u4vn2g7ucidc&redirect_uri=https://main.d265g0svwku80s.amplifyapp.com&scope=openid&code_challenge_method=S256&code_challenge=ysTjhkklBZuCfk3DzUWfAc8_e_-7fntxl7Ggqp2oWWM";
+}
